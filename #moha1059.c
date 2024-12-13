63b111e8f4a082175c3d953dbf2db1e6644fd75a
Hamza Mohamed <157754598+Adeyshamza@users.noreply.github.com>
2024-12-12 23:14:46 -0600

    Add files via upload

---
+++ #moha1059.c
@@ -0,0 +1,15 @@
+#moha1059
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    struct address{
+        char street[100],
+        char state[2],
+        int zip;
+    }
+};
+struct address myAddress;
+int i;
+//myAddress = "810 thornton st se";
